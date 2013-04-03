bc6edfde240886e77255d6db0ea0fed76930db74
Agnes van Belle <agnesvanbelle@gmail.com>
2013-04-03 21:20:57 +0200

    about splash

--- data.php
+++ data.php
@@ -14,7 +14,9 @@
         <script type="text/javascript" src="sessvars.js"></script>
         <!--<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>-->
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+         
         
+
         
         <script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
         <!-- <script type="text/javascript" src="fisheye.js"></script> -->
@@ -34,38 +36,53 @@
         
         <script type="text/javascript" src="chosen.js"></script>
 
-        
-
+        <script type="text/javascript" src="leanModal.js"></script>
+   
                 
         <link type="text/css" rel="stylesheet" href="chosen1.css" />
         <link type="text/css" rel="stylesheet" href="jqvmap/jqvmap.css" />
         <link href="indexstyle.css" rel="stylesheet" type="text/css" />
         
          <link type="text/css" rel="stylesheet" href="radarstyle.css" />
+           
+        
+        
         
         
     </head>
     <body>
     
-   
+    
+
     <a name="firsttabsection"></a>  
-    <p id="about"><a class="about" href="about.html">About</a></p>
+   
     <div id="header">
         
 		<img src="images/header.png" alt="">
         
         </br>
-        <p class="info">Compare the number of hours you have to work for a product per country on the minimum wage of that country.</br>
+        <p class="info2">Compare the number of hours you have to work for a product per country on the minimum wage of that country.</br>
 		See for more information about the product prices <a href="http://www.numbeo.com">this site</a>.</br></p>
-        
+        <a  href="#lean_overlay" id="aboutlink" class="about">About</a>
         
     </div>
-	 
-	<div id="belt"></div>
+	   <div id="belt"></div>
+      
+	
         
 	<div id="wrapper">
         
         <div id="firsttab" >
+        
+                 <div id="lean_overlay">
+                    <p id="aboutheader"> About </p>
+                    <p class="abouttext">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+                        </br></br>
+                        Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.
+                    </p>
+                </div>
+    
                 <p class="info">Click on a product icon below the map too see the workload with that product added. </br> Select countries first to compare them individually.</p>
 
                 <div id="my_jqvmap" class="jqvmap"></div> <!-- world map-->
@@ -300,6 +317,9 @@
                                         barChartOnce();
                                         
                                         $('#secondtab').css("display", "none"); 
+                                        
+                                        $("#aboutlink").leanModal();
+
                                     
                                 });
                             });
@@ -339,6 +359,7 @@
     </div> <!-- id = wrapper -->
     
    
+    
     
     </body>
 </html>     
